a6c38deefd8b960372c76913f3436f18a4986da8
Ana Carolina Matosinhos Porto <carolinamatosinhos98@gmail.com>
2022-09-19 17:09:52 -0300

    Jogo da velha

---
+++ scripts/interface.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => { //vai ser executado quando o documento for carregado
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+})
+
+function handleClick(event) {
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert("O jogo acabou. O vencedor foi " + playerTime);
+        }, 10);
+    };
+    updateSquare(position);
+}
+
+function updateSquare(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+function resetGame () {
+    
+}
